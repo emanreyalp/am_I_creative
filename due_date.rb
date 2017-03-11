b9c2dfc96a92d4e2f9741c95293064d3eb82e9c0
G <domok.gergely+github@gmail.com>
2017-03-11 09:53:20 +0100

    First test cases

--- due_date.rb
+++ due_date.rb
@@ -1,22 +1,32 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 class DueDate
   def initialize(submit_date)
     @submit_date = submit_date
     @ret = submit_date
+
+    return @ret
   end
 
   def add_time(turnaround_time)
     hours = turnaround_time.split(':')[0].to_i
     mins = turnaround_time.split(':')[1].to_i
 
-    add_minutes(mins)
-    add_hours(hours)
+    @ret = add_minutes(mins)
+    @ret = add_hours(hours)
+    return get_time
   end
 
   def add_minutes(min)
-
+    @ret + min*60
   end
 
   def add_hours(hour)
+    @ret + hour
+  end
 
+  def get_time
+    return @ret
   end
 end
--- test_due_date.rb
+++ test_due_date.rb
@@ -1,10 +1,19 @@
+#!/bin/env ruby
+# encoding: utf-8
+
 require_relative "due_date"
 require "test/unit"
 
 class TestDueDate < Test::Unit::TestCase
   def test_simple
     res = DueDate.new(Time.local(2016,05,26,12,13)).add_time("0:50")
-    exp = Time.local(2016,05,6,13,03)
+    exp = Time.local(2016,05,26,13,03)
     assert_equal(exp,res)
+  end
+
+  def test_so_simple
+    res = DueDate.new(Time.local(2016,05,6,13,03))
+    exp = Time.local(2016,05,6,13,03)
+    assert_equal(exp,res.get_time)
   end
 end
