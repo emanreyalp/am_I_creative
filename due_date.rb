8105853a8f34685ef34678becc9eab84f62b3809
G <domok.gergely+github@gmail.com>
2018-01-11 22:26:23 +0100

    Refactor with rubocop auto-correction

--- due_date.rb
+++ due_date.rb
@@ -14,20 +14,18 @@ class DueDate
     @time = add_minutes(mins)
     @time = add_hours(hours)
     get_time
-    return DueDate.new(@time).get_time
+    DueDate.new(@time).get_time
   end
 
   def get_time
     @time
   end
 
-private
+  private
 
   def add_minutes(work_min)
     @time += work_min * minute_in_seconds
-    if(@time.hour >= 17)
-      overflow_a_day
-    end
+    overflow_a_day if @time.hour >= 17
     @time
   end
 
@@ -35,22 +33,18 @@ private
     add_days(work_hour / 8)
     work_hour -= (work_hour / 8) * 8
     @time += work_hour * hour_in_seconds
-    if(@time.hour >= 17)
-      overflow_a_day
-    end
-    if(@time.saturday?)
-      overflow_a_weekend
-    end
+    overflow_a_day if @time.hour >= 17
+    overflow_a_weekend if @time.saturday?
     @time
   end
 
   def add_days(work_day)
     if work_day < 5
-      return @time += work_day * day_in_seconds
+      @time += work_day * day_in_seconds
     else
-      week = work_day/5
-      work_day -= week*5
-      return @time += (week*7 + work_day) * day_in_seconds
+      week = work_day / 5
+      work_day -= week * 5
+      @time += (week * 7 + work_day) * day_in_seconds
     end
   end
 
--- test_due_date.rb
+++ test_due_date.rb
@@ -1,73 +1,73 @@
 #!/bin/env ruby
 # encoding: utf-8
 
-require_relative "due_date"
-require "test/unit"
+require_relative 'due_date'
+require 'test/unit'
 
 class TestDueDate < Test::Unit::TestCase
   def test_initialize
-    res = DueDate.new(Time.local(2016,05,6,13,03))
-    exp = Time.local(2016,05,6,13,03)
-    assert_equal(exp,res.get_time)
+    res = DueDate.new(Time.local(2016, 5, 6, 13, 3))
+    exp = Time.local(2016, 5, 6, 13, 3)
+    assert_equal(exp, res.get_time)
   end
 
   def test_add_minutes
-    res = DueDate.new(Time.local(2016,05,26,12,13)).add_time("0:40")
-    exp = Time.local(2016,05,26,12,53)
+    res = DueDate.new(Time.local(2016, 5, 26, 12, 13)).add_time('0:40')
+    exp = Time.local(2016, 5, 26, 12, 53)
     assert_equal(exp, res)
   end
 
   def test_add_hours
-    res = DueDate.new(Time.local(2017,3,9,15,40)).add_time("1:00")
-    exp = DueDate.new(Time.local(2017,3,9,16,40)).get_time
+    res = DueDate.new(Time.local(2017, 3, 9, 15, 40)).add_time('1:00')
+    exp = DueDate.new(Time.local(2017, 3, 9, 16, 40)).get_time
     assert_equal(exp, res)
   end
 
   def test_hour_overflow_cause_of_the_minutes
-    res = DueDate.new(Time.local(2017,3,9,15,40)).add_time("0:30")
-    exp = DueDate.new(Time.local(2017,3,9,16,10)).get_time
+    res = DueDate.new(Time.local(2017, 3, 9, 15, 40)).add_time('0:30')
+    exp = DueDate.new(Time.local(2017, 3, 9, 16, 10)).get_time
     assert_equal(exp, res)
   end
 
   def test_add_hours_and_minutes
-    res = DueDate.new(Time.local(2017,3,9,15,40)).add_time("1:10")
-    exp = DueDate.new(Time.local(2017,3,9,16,50)).get_time
+    res = DueDate.new(Time.local(2017, 3, 9, 15, 40)).add_time('1:10')
+    exp = DueDate.new(Time.local(2017, 3, 9, 16, 50)).get_time
     assert_equal(exp, res)
   end
 
   def test_day_overflow_cause_of_the_minutes
-    res = DueDate.new(Time.local(2017,3,9,16,40)).add_time("0:30")
-    exp = DueDate.new(Time.local(2017,3,10,9,10)).get_time
+    res = DueDate.new(Time.local(2017, 3, 9, 16, 40)).add_time('0:30')
+    exp = DueDate.new(Time.local(2017, 3, 10, 9, 10)).get_time
     assert_equal(exp, res)
   end
 
   def test_day_overflow_cause_of_the_hours
-    res = DueDate.new(Time.local(2017,3,9,16,40)).add_time("1:00")
-    exp = DueDate.new(Time.local(2017,3,10,9,40)).get_time
+    res = DueDate.new(Time.local(2017, 3, 9, 16, 40)).add_time('1:00')
+    exp = DueDate.new(Time.local(2017, 3, 10, 9, 40)).get_time
     assert_equal(exp, res)
   end
 
   def test_day_overflow_cause_of_a_lot_of_hours
-    res = DueDate.new(Time.local(2017,3,6,13,40)).add_time("11:00")
-    exp = DueDate.new(Time.local(2017,3,7,16,40)).get_time
+    res = DueDate.new(Time.local(2017, 3, 6, 13, 40)).add_time('11:00')
+    exp = DueDate.new(Time.local(2017, 3, 7, 16, 40)).get_time
     assert_equal(exp, res)
   end
 
   def test_add_two_week_of_hours
-    res = DueDate.new(Time.local(2017,3,9,16,40)).add_time("80:00")
-    exp = DueDate.new(Time.local(2017,3,23,16,40)).get_time
+    res = DueDate.new(Time.local(2017, 3, 9, 16, 40)).add_time('80:00')
+    exp = DueDate.new(Time.local(2017, 3, 23, 16, 40)).get_time
     assert_equal(exp, res)
   end
 
   def test_add_two_week_of_hours_and_overflow_cause_of_the_minutes
-    res = DueDate.new(Time.local(2017,3,9,16,40)).add_time("80:30")
-    exp = DueDate.new(Time.local(2017,3,24,9,10)).get_time
+    res = DueDate.new(Time.local(2017, 3, 9, 16, 40)).add_time('80:30')
+    exp = DueDate.new(Time.local(2017, 3, 24, 9, 10)).get_time
     assert_equal(exp, res)
   end
 
   def test_week_overflow_cause_of_the_hours
-    res = DueDate.new(Time.local(2017,3,10,15,0)).add_time("3:00")
-    exp = DueDate.new(Time.local(2017,3,13,10,0)).get_time
+    res = DueDate.new(Time.local(2017, 3, 10, 15, 0)).add_time('3:00')
+    exp = DueDate.new(Time.local(2017, 3, 13, 10, 0)).get_time
     assert_equal(exp, res)
   end
 end
