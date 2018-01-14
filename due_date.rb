55badb62ce7587db653f8775d864817a13ad6210
G <domok.gergely+github@gmail.com>
2018-01-14 23:50:46 +0100

    Use core_ext gem for dst and time quantities management

--- due_date.rb
+++ due_date.rb
@@ -1,6 +1,8 @@
 #!/bin/env ruby
 # encoding: utf-8
 
+require "core_ext/time"
+
 class DueDate
   def initialize(submit_date)
     @submit_date = submit_date
@@ -13,10 +15,8 @@ class DueDate
     hours = turnaround_time.split(':')[0].to_i
     mins = turnaround_time.split(':')[1].to_i
 
-    @time = add_minutes(mins)
-    @time = add_hours(hours)
-
-    check_dst(dst_before, @time.dst?)
+    add_minutes(mins)
+    add_hours(hours)
 
     time?
   end
@@ -28,7 +28,7 @@ class DueDate
   private
 
   def add_minutes(work_min)
-    @time += work_min * minute_in_seconds
+    @time += work_min.minute
     check_overflow!
     @time
   end
@@ -36,37 +36,27 @@ class DueDate
   def add_hours(work_hour)
     add_days(work_hour / 8)
     work_hour -= (work_hour / 8) * 8
-    @time += work_hour * hour_in_seconds
+    @time += work_hour.hour
     check_overflow!
     @time
   end
 
   def add_days(work_day)
     if work_day < 5
-      @time += work_day * day_in_seconds
+      @time += work_day.day
     else
       week = work_day / 5
       work_day -= week * 5
-      @time += (week * 7 + work_day) * day_in_seconds
+      @time += (week * 7 + work_day).day
     end
   end
 
   def overflow_a_day
-    @time += 16 * hour_in_seconds
+    @time += 16.hour
   end
 
   def overflow_a_weekend
-    @time += 2 * day_in_seconds
-  end
-
-  def check_dst(dst_before, dst_now)
-    return if dst_before == dst_now
-
-    if dst_before
-      add_time('1:00')
-    else
-      add_time('-1:00')
-    end
+    @time += 2.day
   end
 
   def check_overflow!
@@ -74,15 +64,4 @@ class DueDate
     overflow_a_weekend if @time.saturday?
   end
 
-  def minute_in_seconds
-    60
-  end
-
-  def hour_in_seconds
-    60 * minute_in_seconds
-  end
-
-  def day_in_seconds
-    24 * hour_in_seconds
-  end
 end
